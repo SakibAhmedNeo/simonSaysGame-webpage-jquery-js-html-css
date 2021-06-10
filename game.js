818407690c1435b914faa6e89ce5b4b44528426a
SakibAhmedNeo <52414438+SakibAhmedNeo@users.noreply.github.com>
2021-06-10 08:37:36 +0600

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
 }
 
